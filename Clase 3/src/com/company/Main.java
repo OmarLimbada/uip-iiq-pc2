000052d7aec49ccfe043d8b2682d749861c89ca8
OmarLimbada <omar_limbada@hotmail.com>
2017-09-22 21:39:03 -0500

    22/9/17
    clase 3

---
+++ Clase 3/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Java");
+
+        //ENTEROS
+        byte a;
+        short b;
+        int c = 27;
+        long d;
+
+        //DECIMALES
+        float e;
+        double f = 0.0;
+
+        //TEXTO
+        char g = 'x';
+        String i = "x";
+
+        //BOOLEANO
+        boolean vivo = true;
+        vivo = false;
+    }
+}
